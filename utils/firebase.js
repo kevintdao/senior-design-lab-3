5e2e9dc99c98aab87cb05ce0368532ad3e2b0d59
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-15 20:57:00 -0600

    Add firebase config

---
+++ utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const app = firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+})
