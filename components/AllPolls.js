adf7fcb9e0df060926026fc89ae6fe0dd2024e4b
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-23 22:48:21 -0600

    Add active/past polls table in dashboard

--- components/AllPolls.js
+++ components/AllPolls.js
@@ -5,13 +5,14 @@ import { db } from '../utils/firebase'
 
 export default function AllPolls() {
     const { currentUser } = useAuth();
-    const [polls, setPolls] = useState([]);
     const [loading, setLoading] = useState(true);
     const currentDate = new Date(); 
+    const [activePolls, setActivePolls] = useState([]);
+    const [pastPolls, setPastPolls] = useState([]);
 
     async function getPolls(email){
         const output = [];
-        const q = query(collection(db, "polls"), where("email", "==", email), orderBy("deadline"));
+        const q = query(collection(db, "polls"), where("email", "==", email), orderBy("end"));
         const snapshot = await getDocs(q);
         snapshot.forEach((doc) => {
             output.push({
@@ -23,8 +24,19 @@ export default function AllPolls() {
     }
 
     useEffect(() => {
+        var active = [];
+        var past = [];
         getPolls(currentUser.email).then(response => {
-            setPolls(response);
+            response.forEach((poll) => {
+                if (currentDate < poll.data.end.toDate()){
+                    active.push(poll);
+                }
+                else{
+                    past.push(poll);
+                }
+            })
+            setActivePolls(active);
+            setPastPolls(past);
             setLoading(false);
         })
     }, [])
@@ -33,25 +45,58 @@ export default function AllPolls() {
         return <div></div>
     }
 
-    // no polls
-    if(polls.length == 0) {
-        console.log(currentDate);
-        return <div className="flex md:flex-row md:justify-between mt-2 justify-start flex-col">
-            <div className="border rounded-md mb-2 w-full mr-4 p-2">
-                <h5>Active Poll(s)</h5>
-                <p>No Active Poll</p>
-            </div>
+    function Polls(props){
+        const polls = props.polls
+        if(polls.length == 0){
+            return (
+                <p>No Active Poll(s)</p>
+            )
+        }
+        else{
+            return (
+                <table className="divide-y divide-gray-200 min-w-full">
+                    <thead className="bg-gray-50">
+                        <tr>
+                            <th>Title</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>More Info</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                        {polls.map((poll, i) => {
+                            const [sDay, sMonth, sDate, sYear] = poll.data.start.toDate().toString().split(" ");
+                            const [eDay, eMonth, eDate, eYear] = poll.data.end.toDate().toString().split(" ");
+                            const pollLink = `/poll/${poll.id}`
 
-            <div className="border rounded-md mb-2 w-full p-2">
-                <h5>Past Poll(s)</h5>
-                <p>No Active Poll</p>
-            </div>
-        </div>
+                            return (
+                                <tr key={i}>
+                                    <td>{poll.data.title}</td>
+                                    <td>{sMonth} {sDate} {sYear}</td>
+                                    <td>{eMonth} {eDate} {eYear}</td>
+                                    <td><a href={pollLink}>View</a></td>
+                                    <td>Edit</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            )
+        }
     }
 
     return (
-        <div>
-            {/* {polls[0].data.deadline.toDate().toString()} */}
+        <div className="flex md:flex-row md:justify-between mt-2 justify-start flex-col">
+            <div className="border rounded-md mb-2 w-full mr-4 p-2">
+                <h5 className="text-center mb-2">Active Poll(s)</h5>
+                <Polls polls={activePolls} />
+            </div>
+
+            <div className="border rounded-md mb-2 w-full p-2">
+                <h5 className="text-center">Past Poll(s)</h5>
+                <Polls polls={pastPolls} />
+            </div>
         </div>
     )
 }
