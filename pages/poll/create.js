690f8cfe63f8b8de2e236b1dfd69df6b9b78bd0e
Fatima Mohamed <fmmohamed@uiowa.edu>
2021-11-24 20:12:23 -0600

    added some functionality to the handleSubmit function to check for invalid inputs. Still need to add functionality to the + button, issue with displaying error messages by clickingany button.

--- pages/poll/create.js
+++ pages/poll/create.js
@@ -1,11 +1,13 @@
+import { isEmpty } from '@firebase/util';
 import React, { useRef, useState } from 'react'
 import PrivateRoute from '../../components/PrivateRoute'
+import Alert from '../../components/Alert'
 
 export default function create() {
     const title = useRef();
     const location = useRef();
     const notes = useRef();
-    const dates = useRef();
+    const date = useRef();
     const start = useRef();
     const end = useRef();
     const [loadingPlus, setLoadingPlus] = useState(false);
@@ -13,39 +15,60 @@ export default function create() {
     const numSlot = useRef();
     const numPerson = useRef();
     const [loadingPublish, setLoadingPublish] = useState(false);
+    const deadlineDate = useRef();
+    const deadlineTime = useRef();
+    const [error, setError] = useState('');
 
     async function handleSubmit(e){
-        // e.preventDefault();
+        e.preventDefault();
+        var errorMsg = "";
+        
+        if(isEmpty(title.current.value)){
+            errorMsg += "Enter a title!\n\n"
+        }
+        
+        if (isEmpty(date.current.value)){
+            errorMsg += "Enter a date!\n"
+        }
 
-        // if(passwordRef.current.value !== passwordConfirmRef.current.value){
-        //     return setError('Passwords do not match')
-        // }
+        if (isEmpty(start.current.value)){
+            errorMsg += "Enter a start time!\n"
+        }
 
-        // if(passwordRef.current.value.length < 6){
-        //     return setError('Password should be at least 6 characters')
-        // }
+        if (isEmpty(end.current.value)){
+            errorMsg += "Enter an end time!\n"
+        }
 
-        // try{
-        //     setError('')
-        //     // prevent user from creating multiple accounts by clicking signup button repeatedly
-        //     setLoading(true)
-        //     await signup(emailRef.current.value, passwordRef.current.value).then(async (userCredential) => {
-        //         await updateName(userCredential.user, fnameRef.current.value, lnameRef.current.value)
-        //     })
-        //     router.push('/dashboard');
-        // } catch(e) {
-        //     console.log(e);
-        //     setError('Failed to create an account');
-        // }
+        if (!document.getElementById('slot').checked && !document.getElementById('block').checked){
+            errorMsg += "Make a selection: Number of Blocks or Minutes per Time Slot!\n"
+        }
+
+        if (document.getElementById('slot').checked && numSB.current.value < 5){
+            errorMsg += "Time Slots must be at least 5 minutes!\n"
+        }
+
+        if (isEmpty(numSB.current.value)){
+            errorMsg += "Enter a number!\n"
+        }
+
         setLoadingPlus(false);
         setLoadingPublish(false);
+        return setError(errorMsg);
     }
+
+    function myFunction() {
+        document.getElementById("myDIV").innerHTML="myDIV";
+    }
+
 
     return PrivateRoute(
         <div>
             <form action="#" method="POST" onSubmit={handleSubmit} className="flex items-center justify-center py-2">
                 <div className="w-1/2 max-w-md space-y-2 p-4 rounded-md bg-white shadow-md border border-gray-200 mt-10">
                     
+                    {/* error alert */}
+                    {error && <Alert text={error} />}
+
                     <div className="flex flex-col">
                         <label htmlFor="create">Title*</label>
                         <input type="text" id="title" name="title" ref={title} className="border border-gray-300 rounded p-2"/>
@@ -63,23 +86,23 @@ export default function create() {
 
                     <div className="flex flex-col">
                         <label htmlFor="create">Date*</label>
-                        <input type="date" id="date" name="date" ref={dates} className="border border-gray-300 rounded p-2"/>
+                        <input type="date" id="date" name="date" ref={date} className="border border-gray-300 rounded p-2"/>
                         <p>Start Time*</p>
                         <input type="time" id="start" name="start" ref={start} className="border border-gray-300 rounded p-2"/>
                         <p>End Time*</p>
                         <input type="time" id="end" name="end" ref={end} className="border border-gray-300 rounded p-2"/>                   
-                        <button className="h-8 w-full mt-4 rounded-md flex items-center justify-center bg-indigo-600 text-white hover:bg-indigo-700 p-2" onclick="myFunction()">+</button>
-                        <div id="myDIV"> + </div>
+                        <button onclick={myFunction} disabled={loadingPlus} className="h-8 w-full mt-4 rounded-md flex items-center justify-center bg-indigo-600 text-white hover:bg-indigo-700 p-2">+</button>
                     </div>
 
                     <div className="flex flex-col">
                         <p>Choose one*</p>
                         <div>
-                            <input type="radio" id="slots" name="slots" className="border border-gray-300 rounded p-2" value="Slots"/>
+                            <input type="radio" id="block" name="SB" className="border border-gray-300 rounded p-2" value="block"/>
                             <label htmlFor="create"> Number of Blocks</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="slots" name="slots" className="border border-gray-300 rounded p-2" value="Slots"/>
+                            <p>
+                                
+                            </p>
+                            <input type="radio" id="slot" name="SB" className="border border-gray-300 rounded p-2" value="slot"/>
                             <label htmlFor="create"> Minutes per Time Slot</label>
                         </div>
 
@@ -114,12 +137,12 @@ export default function create() {
 
                     <div className="flex flex-col">
                         <p>Deadline for the poll</p>
-                        <input type="date" id="deadlineDate" name="deadlineDate" ref={dates} className="border border-gray-300 rounded p-2"/>
-                        <input type="time" id="deadlineTime" name="deadlineTime" ref={start} className="border border-gray-300 rounded p-2"/>
+                        <input type="date" id="deadlineDate" name="deadlineDate" ref={deadlineDate} className="border border-gray-300 rounded p-2"/>
+                        <input type="time" id="deadlineTime" name="deadlineTime" ref={deadlineTime} className="border border-gray-300 rounded p-2"/>
                     </div>
 
-                    <div className="flex flex-col">
-                        <button disabled={loadingPublish} className="h-8 w-full mt-4 rounded-md flex items-center justify-center bg-indigo-600 text-white hover:bg-indigo-700 p-2">Publish</button>
+                    <div>
+                        <button className="h-8 w-full mt-4 rounded-md flex items-center justify-center bg-indigo-600 text-white hover:bg-indigo-700 p-2">Publish</button>
                     </div>
 
                 </div>
