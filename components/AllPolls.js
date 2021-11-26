c8e834baa6d61cc49e6fb6ac18bcecee88390d6e
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-26 10:50:08 -0600

    Add invite users functionality using email

--- components/AllPolls.js
+++ components/AllPolls.js
@@ -3,6 +3,7 @@ import { useAuth } from '../AuthContext'
 import { query, collection, where, orderBy, getDocs } from '@firebase/firestore'
 import { db } from '../utils/firebase'
 import Popup from 'reactjs-popup'
+import Alert from './Alert'
 import 'reactjs-popup/dist/index.css'
 
 export default function AllPolls() {
@@ -11,6 +12,8 @@ export default function AllPolls() {
     const currentDate = new Date();
     const [activePolls, setActivePolls] = useState([]);
     const [pastPolls, setPastPolls] = useState([]);
+    const [alert, setAlert] = useState('');
+    const [error, setError] = useState('');
 
     async function getPolls(email) {
         const output = [];
@@ -108,14 +111,35 @@ export default function AllPolls() {
             </Popup>
         )
     }
-    function sendEmail(id) {
-        const users = document.getElementById("users").value.split("\n");
-        console.log(users);
-        console.log(id);
+
+    async function sendEmail(id) {
+        const users = document.getElementById("users").value.trim().split("\n");
+        const formData = {};
+
+        formData['id'] = id;
+        formData['users'] = users;
+        console.log(formData);
+        const response = await fetch('/api/mail', {
+            method: 'POST',
+            body: JSON.stringify(formData)
+        })
+        const data = await response.json();
+        
+        setAlert('');
+        setError('');
+        if(data.status == 'Ok'){
+            setAlert('Invites successfully sent!');
+        }
+        else{
+            setError('Failed to sent invitation');
+        }
     }
 
     return (
         <div className="flex mt-2 justify-start flex-col">
+            {alert && <Alert text={alert} bgColor={'bg-green-100'} textColor={'text-green-700'} borderColor={'border-green-400'} />}
+            {error && <Alert text={error} />}
+
             <div className="border rounded-md mb-2 w-full p-2 bg-white shadow-sm">
                 <h5 className="text-center mb-2">Active Poll(s)</h5>
                 <Polls polls={activePolls} />
---
+++ pages/api/mail.js
@@ -0,0 +1,27 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+
+export default function mailAPI(req, res) {
+  const body = JSON.parse(req.body)
+  
+  const message = `
+    You got invited to vote in a poll.\r\n
+    Click the link below to participate\r\n\r\n
+    https://senior-design-lab-3.vercel.app/poll/${body.id}
+  `;
+
+  const mail = {
+    to: body.users,
+    from: 'teamnull2021@gmail.com',
+    subject: 'Invitation to vote in a poll',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  };
+
+  sgMail.sendMultiple(mail).then(() => {
+    console.log("emails sent successfully!");
+    res.status(200).json({ status: 'Ok' })
+  }).catch(error => {
+    console.log(error);
+  })
+}
