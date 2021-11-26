0015dd89d5412620bcc7a5da4c69a497900a0875
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-26 12:47:14 -0600

    Change max-w

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,7 +6,7 @@ export default function dashboard() {
     return PrivateRoute(
         <div>
             <div className="bg-white shadow">
-                <h1 className="text-4xl font-bold text-gray-900 py-3 container max-w-4xl lg:mx-auto">Dashboard</h1>
+                <h1 className="text-4xl font-bold text-gray-900 py-3 container max-w-6xl lg:mx-auto">Dashboard</h1>
             </div>
             <div className="container max-w-4xl lg:mx-auto">
                 <AllPolls />
