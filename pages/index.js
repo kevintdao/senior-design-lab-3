ac06f30b4cbe6ea12901177de259b66972393ba9
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-26 12:52:56 -0600

    Change max-w

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function index() {
       <div className="bg-white shadow">
         <h1 className="text-4xl font-bold text-gray-900 py-3 container max-w-6xl lg:mx-auto">Team NULL Poll System</h1>
       </div>
-      <div className="container max-w-4xl lg:mx-auto">
+      <div className="container max-w-6xl lg:mx-auto">
         <h3 className="text-xl font-bold text-gray-900 py-3">A better version of Doodle (hopefully...)</h3>
       </div>
     </div>
