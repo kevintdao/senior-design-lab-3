fc8dc42171a862ab12564fd17fc22a86682d4c3e
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-30 15:30:04 -0600

    Add function to create dates

--- pages/poll/create.js
+++ pages/poll/create.js
@@ -92,35 +92,56 @@ export default function create() {
             let start = block.querySelector('[id^="start"]').value;
             let end = block.querySelector('[id^="end"]').value;
 
-            splitTime(start, end, 'blocks', 5);
-            splitTime(start, end, 'slots', 15);
+            splitTime(date, start, end, 'blocks', 5);
+            splitTime(date, start, end, 'slots', 15);
         })
 
         return [];
     }
 
-    function splitTime(start, end, type, number) {
+    function splitTime(date, start, end, type, number) {
         let [startHour, startMin] = start.split(':');
         let [endHour, endMin] = end.split(':');
         
-        let startTime = startHour * 60 + (startMin - 240);
-        let endTime = endHour * 60 + (endMin - 240);
-        
+        let startTime = startHour * 60 + (startMin - 0);
+        let endTime = endHour * 60 + (endMin - 0);
+
         let blockDuration = endTime - startTime;
 
         let output = [];
         if(type == 'blocks'){
             let eachBlockDuration = blockDuration / number;
             
-            for(let i = 0; i < number; i++){
-                // convert to hour and min
-                let blockStart = startTime;
-            }
+            output = createDates(startTime, endTime, eachBlockDuration, number, date);
         }
         else{
             let numOfSlot = blockDuration / number;
-            console.log(numOfSlot);
+            
+            output = createDates(startTime, endTime, number, numOfSlot, date);
         }
+        console.log(output);
+    }
+
+    function createDates(startTime, endTime, duration, slots, date){
+        let [year, month, day] = date.split('-');
+        console.log(duration);
+
+        let output = [];
+        for(let i = 0; i < slots; i++){
+            let blockStart = startTime + duration * i;
+            let blockEnd = startTime + duration * (i + 1);
+
+            let blockStartHour = Math.floor(blockStart / 60);
+            let blockStartMin = blockStart - blockStartHour * 60;
+            let blockEndHour = Math.floor(blockEnd / 60);
+            let blockEndMin = blockEnd - blockEndHour * 60;
+
+            output.push({
+                start: new Date(year, month - 1, day, blockStartHour, blockStartMin),
+                end: new Date(year, month - 1, day, blockEndHour, blockEndMin)
+            });
+        }
+        return output;
     }
 
     function addDate() {
