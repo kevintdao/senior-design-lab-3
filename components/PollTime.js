72061d232b0d2da17ffeaeccb7df3b38104490a1
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-29 23:46:33 -0600

    Add insert poll info

--- components/PollTime.js
+++ components/PollTime.js
@@ -2,25 +2,22 @@ import React, { useRef } from 'react'
 
 export default function PollTime(props) {
     const index = props.index;
-    const date = useRef();
-    const start = useRef();
-    const end = useRef();
 
     return (
         <div className="flex md:flex-row md:space-x-2 justify-between flex-col">
             <div className="flex flex-col w-full">
                 <label htmlFor="date">Date*</label>
-                <input type="date" id={`date-${index}`} name="date" ref={date} className="border border-gray-300 rounded p-2" />
+                <input type="date" id={`date-${index}`} name="date" className="border border-gray-300 rounded p-2" />
             </div>
 
             <div className="flex flex-col w-full">
                 <label htmlFor="start">Start Time*</label>
-                <input type="time" id={`start-${index}`} name="start" ref={start} className="border border-gray-300 rounded p-2" />
+                <input type="time" id={`start-${index}`} name="start" className="border border-gray-300 rounded p-2" />
             </div>
             
             <div className="flex flex-col w-full">
                 <label htmlFor="end">End Time*</label>
-                <input type="time" id={`end-${index}`} name="end" ref={end} className="border border-gray-300 rounded p-2" />
+                <input type="time" id={`end-${index}`} name="end" className="border border-gray-300 rounded p-2" />
             </div>
         </div>
     )
--- pages/poll/create.js
+++ pages/poll/create.js
@@ -1,25 +1,26 @@
 import React, { useRef, useState } from 'react'
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { db } from '../../utils/firebase'
 import PrivateRoute from '../../components/PrivateRoute'
 import Alert from '../../components/Alert'
 import PollTime from '../../components/PollTime';
+import { useAuth } from '../../AuthContext'
 
 export default function create() {
-    const title = useRef();
-    const location = useRef();
-    const notes = useRef();
-    const date = useRef();
-    const start = useRef();
-    const end = useRef();
-    const numSB = useRef();
-    const numSlot = useRef();
-    const numPerson = useRef();
-    const deadlineDate = useRef();
-    const deadlineTime = useRef();
+    const titleRef = useRef();
+    const locationRef = useRef();
+    const notesRef = useRef();
+    const numSBRef = useRef();
+    const numSlotRef = useRef();
+    const numPersonRef = useRef();
+    const deadlineDateRef = useRef();
+    const timezoneRef = useRef();
     const [error, setError] = useState('');
     const [dateList, setDateList] = useState([]);
+    const { currentUser } = useAuth();
 
     async function handleSubmit(e) {
-        // e.preventDefault();
+        e.preventDefault();
         // var errorMsg = "";
 
         // if (isEmpty(title.current.value)) {
@@ -51,6 +52,34 @@ export default function create() {
         // }
 
         //return setError(errorMsg);
+
+        // add new document for poll
+        const newPollRef = doc(collection(db, 'polls'));
+        insertPoll(newPollRef);
+    }
+
+    async function insertPoll(newPollRef) {
+        let [year, month, date] = deadlineDateRef.current.value.split('-');
+
+        await setDoc(newPollRef, {
+            email: currentUser.email,
+            end: new Date(year, month - 1, date, 0, 0, 0),
+            location: locationRef.current.value,
+            notes: notesRef.current.value,
+            start: new Date(),
+            timezone: timezoneRef.current.value,
+            title: titleRef.current.value,
+            votes_per_slot: numSlotRef.current.value,
+            votes_per_user: numPersonRef.current.value
+        });
+    }
+
+    function insertBlock() {
+
+    }
+
+    function insertTime() {
+
     }
 
     // TODO: the functionality of + button 
@@ -58,7 +87,7 @@ export default function create() {
     // and in the render() function use the list to print all of them
     function addDate() {
         setDateList(dateList.concat(
-            <PollTime key={dateList.length} date={date} start={start} end={end} index={dateList.length} />
+            <PollTime key={dateList.length} index={dateList.length} />
         ));
     }
 
@@ -79,30 +108,29 @@ export default function create() {
             {/* error alert */}
             {error && <Alert text={error} />}
 
-            <form action="#" method="POST" onSubmit={handleSubmit} className="container py-2 flex items-center flex-col">
+            <div className="container py-2 flex items-center flex-col">
                 <div className="md:mx-auto w-full rounded-md bg-white shadow-md border border-gray-200 p-4">
                     <div className="flex md:flex-row md:space-x-2 justify-between flex-col">
                         <div className="flex flex-col w-full space-y-1">
                             <label htmlFor="title">Title*</label>
-                            <input type="text" id="title" name="title" ref={title} className="border border-gray-300 rounded p-2" />
+                            <input type="text" id="title" name="title" ref={titleRef} className="border border-gray-300 rounded p-2" />
                         </div>
 
                         <div className="flex flex-col w-full space-y-1">
                             <label htmlFor="location">Location</label>
-                            <input type="text" id="location" name="location" ref={location} className="border border-gray-300 rounded p-2" />
+                            <input type="text" id="location" name="location" ref={locationRef} className="border border-gray-300 rounded p-2" />
                         </div>
 
                         <div className="flex flex-col w-full space-y-1">
                             <label htmlFor="notes">Notes/Comments</label>
-                            <input type="text" id="notes" name="notes" ref={notes} className="border border-gray-300 rounded p-2" />
+                            <input type="text" id="notes" name="notes" ref={notesRef} className="border border-gray-300 rounded p-2" />
                         </div>
                     </div>
 
                     <div className="flex md:flex-row md:space-x-2 justify-between flex-col">
                         <div className="flex flex-col w-full space-y-1">
                             <p>Deadline for the poll</p>
-                            <input type="date" id="deadlineDate" name="deadlineDate" ref={deadlineDate} className="border border-gray-300 rounded p-2" />
-                            <input type="time" id="deadlineTime" name="deadlineTime" ref={deadlineTime} className="border border-gray-300 rounded p-2" />
+                            <input type="date" id="deadlineDate" name="deadlineDate" ref={deadlineDateRef} className="border border-gray-300 rounded p-2" />
                         </div>
 
                         <div className="flex flex-col w-full">
@@ -110,8 +138,8 @@ export default function create() {
 
                         <div className="flex flex-col w-full space-y-1">
                             <label htmlFor="timezone">Time Zone (US)</label>
-                            <select id="timezone" className="border border-gray-300 rounded p-2">
-                                <option value="select">Select One</option>
+                            <select id="timezone" className="border border-gray-300 rounded p-2" ref={timezoneRef}>
+                                <option value=""></option>
                                 <option value="HST">HST</option>
                                 <option value="AKST">AKST</option>
                                 <option value="PST">PST</option>
@@ -138,19 +166,19 @@ export default function create() {
 
                         <div>
                             <label htmlFor="number"> Number* </label>
-                            <input type="number" id="number" name="number" ref={numSB} className="border border-gray-300 rounded p-2" min="1" />
+                            <input type="number" id="number" name="number" ref={numSBRef} className="border border-gray-300 rounded p-2" min="1" />
                         </div>
                     </div>
 
                     <div className="flex md:flex-row md:space-x-2 justify-between flex-col">
                         <div className="flex flex-col w-full space-y-1">
                             <label htmlFor="numSlot">Votes per Slot</label>
-                            <input type="number" id="numSlot" name="numSlot" ref={numSlot} className="border border-gray-300 rounded p-2" min="0" />
+                            <input type="number" id="numSlot" name="numSlot" ref={numSlotRef} className="border border-gray-300 rounded p-2" min="0" />
                         </div>
 
                         <div className="flex flex-col w-full space-y-1">
                             <label htmlFor="numPerson">Votes per Person</label>
-                            <input type="number" id="numPerson" name="numPerson" ref={numPerson} className="border border-gray-300 rounded p-2" min="0" />
+                            <input type="number" id="numPerson" name="numPerson" ref={numPersonRef} className="border border-gray-300 rounded p-2" min="0" />
                         </div>
                     </div>
 
@@ -161,10 +189,10 @@ export default function create() {
                     <AddButton />
 
                     <div>
-                        <button className="h-8 w-full mt-4 rounded-md flex items-center justify-center bg-indigo-600 text-white hover:bg-indigo-700 p-2">Publish</button>
+                        <button onClick={handleSubmit} className="h-8 w-full mt-4 rounded-md flex items-center justify-center bg-indigo-600 text-white hover:bg-indigo-700 p-2">Publish</button>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
