e04e23bac492c1350b2985199c2d6511f0539e21
afilerio <alfredofilerio@gmail.com>
2021-12-01 00:14:52 -0600

    blocks now matches firebase structure. From props.blocks to props.blocks[0].data.blocks

--- components/DisplayPoll.js
+++ components/DisplayPoll.js
@@ -2,7 +2,10 @@ import React from 'react'
 import PollBlock from './PollBlock';
 
 export default function DisplayPoll(props) {
-    const blocks = props.blocks;
+    //const blocks = props.blocks.data.blocks;
+    const blocks = props.blocks[0].data.blocks;
+    console.log("blocks");
+    console.log(blocks);
     const vps = props.vps;
     const vpu = props.vpu;
 
@@ -10,11 +13,14 @@ export default function DisplayPoll(props) {
         <div>
             {blocks.map((block, i) => {
                 const id = block.id;
-                const data = block.data;
+                //const data = block.data;
 
-                const blockDate = data.date.toDate().toString().split(" ");
-                const times = data.time;
-
+                //const blockDate = data.date.toDate().toString().split(" ");
+                console.log("Display");
+                //console.log(data);
+                const blockDate = block.end.toDate().toString().split(" ");
+                //const times = data.time;
+                //return <PollBlock key={i} id={id} times={times} vps={vps} vpu={vpu}/>
                 return <PollBlock key={i} id={id} date={blockDate} times={times} vps={vps} vpu={vpu}/>
             })}
         </div>
