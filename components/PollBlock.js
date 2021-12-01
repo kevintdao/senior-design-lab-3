7f848694b85670e662fb7cc1833583a11aabba4e
afilerio <alfredofilerio@gmail.com>
2021-12-01 00:39:19 -0600

    added checkbox and times to return

--- components/PollBlock.js
+++ components/PollBlock.js
@@ -6,10 +6,10 @@ export default function PollBlock(props) {
     const vps = props.vps;
     const id = props.id;
     var selectedId = [];
-
-    for(let time in times){
-        console.log(time);
-    }
+    console.log(times);
+    // for(let time in times){
+    //     console.log(time);
+    // }
 
     function handleCheckbox(element, vps){
         // TODO: Check for valid number of checkboxes
@@ -31,15 +31,10 @@ export default function PollBlock(props) {
     }
 
     return (
-        <div id={id}>
-            <h6>{day} {month} {date} {year}</h6>
-            {/* {times.map((time, i) => {
-                return <div id={`${id}_${i}`} className="flex items-center" key={i}>
-                    <input id={`${id}_${month}-${date}-${time}`} type="checkbox" className="mr-2" disabled={pollTimes[i] == vps} onChange={(e) => handleCheckbox(e.target, vps)}/>
-                    <p className="w-1/4">{time}</p>
-                    <p>({pollTimes[i]} / {vps})</p>
-                </div>
-            })} */}
+        <div id={id} className="flex items-center">
+            <h6>{day} {month} {date} {year} {times.start} to {times.end}</h6>
+
+            <input type="checkbox" className="mr-2" />
             <hr className="mb-1 mt-1"/>
         </div>
     )
