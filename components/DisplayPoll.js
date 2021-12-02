4e6d8068f6015d6c8cc295e190015516b2ef328e
Kevin Dao <kevin-dao@uiowa.edu>
2021-12-01 21:38:25 -0600

    Fix return bug

--- components/DisplayPoll.js
+++ components/DisplayPoll.js
@@ -7,14 +7,15 @@ export default function DisplayPoll(props) {
     const blocks = props.blocks;
     const vps = props.vps;
     const vpu = props.vpu;
-    const output = [];
     //const id = props.blocks[0].id;
     return (
-        <div>
+        <div id='poll-block'>
             {blocks.map((block) => {
                 const id = block.id;
                 
-                block.data.blocks.map((b, i) => {
+                const output = [];
+
+                return block.data.blocks.map((b, i) => {
                     const blockDateEnd = b.end.toDate().toString().split(" ");
                     const blockDateStart = b.start.toDate().toString().split(" ");
                     const times = {"end": blockDateEnd[4], "start": blockDateStart[4]};
@@ -22,12 +23,19 @@ export default function DisplayPoll(props) {
                     // console.log(i);
                     // console.log(blockDateEnd);
                     output.push(<PollBlock key={i} id={id} date={blockDateEnd} times={times} vps={vps} vpu={vpu} />);
-                    //return <PollBlock key={i} id={id} date={blockDateEnd} times={times} vps={vps} vpu={vpu} />
+                    return <PollBlock key={i} id={id} date={blockDateEnd} times={times} vps={vps} vpu={vpu} />
                 });
+
+                // const index = 0;
+                // output.forEach((block, i ) => {
+                //     console.log(i);
+                //     return (<p>{`index-${index} i${i}`}</p>)
+                // })
+
+                // console.log(block.data.blocks[0].start.toDate());
+
+                // return <p>{block.data.blocks[0].start.toDate().toString()}</p>
                 
-            })}
-            {output.forEach((block,i)=>{
-                console.log(block);
             })}
         </div>
     )
