91969fca35bf6fa6fe9c30e080761dd3db838b23
afilerio <alfredofilerio@gmail.com>
2021-12-02 02:06:30 -0600

    removed comments from PollBlock

--- components/PollBlock.js
+++ components/PollBlock.js
@@ -8,37 +8,8 @@ export default function PollBlock(props) {
     const vpu = props.vpu;
     const id = props.id;
     const votes = props.votes;
-    var selectedId = [];
-    var numSelectedSlots = 0;
-    // for(let time in times){
-    //     console.log(time);
-    // }
-
-    // vps_arr = []
-    // for(let i = 0; i < vps; i++){
-    //     vps_arr.push(0);
-    // }
-
-    // function handleCheckbox(element, vps){
-    //     // TODO: Check for valid number of checkboxes
-    //     var checkboxes = document.querySelectorAll('input[type=checkbox]:not([disabled])');
-    //     var selected = document.querySelectorAll('input[type=checkbox]:checked');
-    //     console.log(selectedId);
-
-    //     if(selected.length <= vps && !selectedId.includes(element.id)){
-    //         selectedId.push(element.id);
-    //         return;
-    //     }
-
-    //     selectedId.shift();
-    //     checkboxes.forEach((checkbox) => {
-    //         if(!selectedId.includes(checkbox.id) && checkbox.checked && checkbox != element ){
-    //             checkbox.checked = false;
-    //         }
-    //     })
-    // }
+    
     function handleCheckbox(element, vps){
-        console.log(element);
         var selected = document.querySelectorAll('input[type=checkbox]:checked');
         if( !(selected.length <= vpu) ){
             selected.forEach((s) => {
