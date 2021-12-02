c6b075664c6995509440f654aaad97fabfc02a1b
afilerio <alfredofilerio@gmail.com>
2021-12-02 02:05:34 -0600

    removed spaces from DisplayPoll

--- components/DisplayPoll.js
+++ components/DisplayPoll.js
@@ -16,7 +16,6 @@ export default function DisplayPoll(props) {
                     const blockDateEnd = b.end.toDate().toString().split(" ");
                     const blockDateStart = b.start.toDate().toString().split(" ");
                     const times = {"end": blockDateEnd[4], "start": blockDateStart[4]};
-                    console.log(block.data.votes[i]);
                     return <PollBlock key={i} bid={i} id={id} date={blockDateEnd} times={times} vps={vps} vpu={vpu} votes ={block.data.votes[i]}/>
                 });
                 
