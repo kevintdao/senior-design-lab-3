27fb42c8f7dc10ae3d1c11f4d1f9fc19900c57fa
afilerio <alfredofilerio@gmail.com>
2021-12-05 15:51:40 -0600

    clean

--- components/DisplayPoll.js
+++ components/DisplayPoll.js
@@ -8,7 +8,6 @@ export default function DisplayPoll(props) {
     const vpu = props.vpu;
     const date = blocks[0].data.blocks[0].start.toDate().toString().split(" ")
 //
-
     return (
         <div id='poll-block'>
             <div>
