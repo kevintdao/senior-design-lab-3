40c06ee77cae3b70346ce78023799e78ee3976d4
afilerio <alfredofilerio@gmail.com>
2021-12-05 16:19:54 -0600

    clean up comment

--- pages/poll/[id].js
+++ pages/poll/[id].js
@@ -174,7 +174,7 @@ export default function Poll() {
                 <input type="text" id='name' className="border border-gray-300 rounded p-2" />
             </div>
             <DisplayPoll blocks={blocks} vps={poll.votes_per_slot} vpu={poll.votes_per_user} />
-//
+
             <button id='submit' className="disabled:bg-indigo-300 disabled:cursor-not-allowed bg-indigo-600 text-white hover:bg-indigo-700 px-3 py-2 rounded-md text-sm font-medium mb-2" onClick={handleSubmit} hidden={currTime > poll.end.seconds ? true : false}>Submit</button>
         </div>
     )
