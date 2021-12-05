68c9acca2ee8069e4625136d34d207eaed9e8f63
afilerio <alfredofilerio@gmail.com>
2021-12-05 16:28:03 -0600

    removed

--- components/DisplayPoll.js
+++ components/DisplayPoll.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PollBlock from './PollBlock';
 
 export default function DisplayPoll(props) {
-    // const block = props.blocks[0].data
+
     const blocks = props.blocks;
     const vps = props.vps;
     const vpu = props.vpu;
