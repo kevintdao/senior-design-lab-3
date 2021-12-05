d661b909cea39f8847ede0c45ed663a519e6451b
afilerio <alfredofilerio@gmail.com>
2021-12-05 16:10:00 -0600

    more removal

--- components/PollBlock.js
+++ components/PollBlock.js
@@ -10,7 +10,6 @@ export default function PollBlock(props) {
     const id = props.id;
     const votes = props.votes;
 
-    
     function handleCheckbox(element, vps){
         var selected = document.querySelectorAll('input[type=checkbox]:checked');
 
@@ -21,7 +20,7 @@ export default function PollBlock(props) {
             alert("You can only select " + vpu + " option(s)!");
         }
     }
-    //
+    
     function formatTime(time){
         const [h, m] = time.split(':')
         
