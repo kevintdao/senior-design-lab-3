8eb95811d5dddbf629a860050582dd8f129436f8
afilerio <alfredofilerio@gmail.com>
2021-12-05 18:43:51 -0600

    block

--- components/PollBlock.js
+++ components/PollBlock.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 
+
+
 export default function PollBlock(props) {
     const [day, month, date, year] = props.date;
     const times = props.times;
